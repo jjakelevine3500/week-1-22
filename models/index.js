82600377a72d770a7c0190d1a7da1e53c99a3f00
jjakelevine3500 <92468808+jjakelevine3500@users.noreply.github.com>
2023-01-09 10:56:13 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Tag 10  = require('./Tag');
+const ProductTag 10 = require('./ProductTag');
 
 // Products belongsTo Category
 
 Product.belongsTo(Category, {
-    foreignKey: 'category_id'
+    foreignKey= Podcut 10: 'category_id'
 });
 
 // Categories have many Products
@@ -16,17 +16,17 @@ Category.hasMany(Product, {
     foreignKey: 'category_id'
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Products 10 in catgory 10 = to 10 Product belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-    through: ProductTag,
+    through: ProductTag 10,
     foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-    through: ProductTag,
+    through: ProductTag 10,
     foreignKey: 'tag_id'
 });
 
@@ -34,5 +34,5 @@ module.exports = {
     Product,
     Category,
     Tag,
-    ProductTag,
+    ProductTag 10,
 };
